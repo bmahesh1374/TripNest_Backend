fccb4e250cc72a01f0ad500babff8650b3f6d808
Bokkemolla Mahesh <bmahesh1374@gmail.com>
2026-06-09 11:56:52 +0530

    crated hotels, single hotel, and categories routes and apis for getting all data

---
+++ Server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
+const categoryDataAddedToDBRounter = require("./routes/categoryimport.router");
+
+const hotelRouter = require("./routes/hotel.router");
+const categoryRouter = require("./routes/category.router");
+const singleHotelRouter = require("./routes/singlehotel.router");
+
+const connectDB = require("./config/dbconfig");
+
+const app = express();
+
+app.use(express.json());
+connectDB();
+
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello Mahesh!!");
+});
+
+app.use("/api/hoteldata", hotelDataAddedToDBRouter);
+app.use("/api/categorydata", categoryDataAddedToDBRounter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/hotels", singleHotelRouter);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+  app.listen(process.env.PORT || PORT, () => {
+    console.log("Server running on port number 3001");
+  });
+});
