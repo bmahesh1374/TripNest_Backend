fc1810b16c97a31de9e7dcc3b5ca827f3b380312
unknown <bmahesh1374@gmail.com>
2026-06-13 09:43:15 +0530

    fix: resolved cors issue

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const cors = require("cors")
 dotenv.config();
 
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
@@ -17,6 +17,7 @@ const connectDB = require("./config/dbconfig");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
